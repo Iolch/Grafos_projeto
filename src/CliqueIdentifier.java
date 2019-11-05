8aa8da1e22b5ff93719f98a6e815613c11a82f2d
Iolanda <iccpaiva@gmail.com>
2019-11-04 18:43:41 -0800

    FEAT: inicia implementação da coloração do grafo

--- src/CliqueIdentifier.java
+++ src/CliqueIdentifier.java
@@ -1,29 +1,50 @@
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 public class CliqueIdentifier {
 	private Graph universe;
-	private ArrayList<Integer> current_clique;
-	private ArrayList<Integer> maximum_clique;
+	private ArrayList<Integer> current_clique = new ArrayList<Integer>();
+	private ArrayList<Integer> maximum_clique = new ArrayList<Integer>();
 	
 	public CliqueIdentifier(Graph u) {
 		this.universe = u;
 	}
-	public ArrayList<Integer> coloringGraph(Graph current_universe){
-		ArrayList<Integer> colorset = new ArrayList<Integer>();
+	public HashMap<Integer,ArrayList<Integer>> coloringGraph(Graph current_universe){
+		HashMap<Integer,ArrayList<Integer>> colorset = new HashMap<Integer,ArrayList<Integer>>();
 		
 		int maxcolor = 1;
-		int kmin = maximum_clique.size() - current_clique.size() + 1;
-		if (kmin <= 0) kmin = 1;
+		int minclass = maximum_clique.size() - current_clique.size() + 1;
+		if (minclass <= 0) minclass = 1;
 		int lastindex = 0;
 		
-		for(int i = 0; i <= current_universe.getNodes().size(); i++)
-		{
-			
-		}
+		colorset.put(1, new ArrayList<Integer>());
+		colorset.put(2, new ArrayList<Integer>());
 		
+		for (Map.Entry<Integer,Integer> node : current_universe.getNodes().entrySet()) {
+			int currentclass = 1;
+			ArrayList<Integer> nodeadjac = getAdjacents(node.getKey());
+			nodeadjac.retainAll(colorset.get(currentclass));
+			while(!nodeadjac.isEmpty()) {
+				currentclass += 1;
+				if(currentclass > maxcolor) {
+					maxcolor = currentclass;
+					colorset.put(maxcolor, new ArrayList<Integer>());
+				}
+				colorset.get(currentclass).add(node.getKey());
+				if(currentclass < minclass) {
+					//TODO
+				}
+			}
+		}
+		colorset.put(lastindex-1, new ArrayList<Integer>());
 		
 		return colorset;
-		
+	}
+	
+	public ArrayList<Integer> getAdjacents(int node){
+		ArrayList<Integer> adjacents = new ArrayList<Integer>();
+		return adjacents;
 	}
 	public ArrayList<Integer> getCurrent_clique() {
 		return current_clique;
--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ public class Main {
 	public static void main(String[] args) {
 		Graph graph = Graph.generateStandardGraph();
 		CliqueIdentifier controller = new CliqueIdentifier(graph);
-		
-		System.out.println(controller.getUniverse().getEdges());
+		controller.coloringGraph(graph);
+		//System.out.println(controller.getUniverse().getEdges());
 	}
 
 }
